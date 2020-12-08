2a15a3ded37848803e36963c761e3551eda733d5
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-08 12:31:52 -0300

    Adiciona consulta de quantidade de ações

--- MySQL/mysql_access.jl
+++ MySQL/mysql_access.jl
@@ -1,8 +1,9 @@
 module DBAccess
     using DataFrames, MySQL;
-    export getTickers, getDF, getAccount;
+    export getTickers, getStocks, getDF, getAccount;
 
     function connectMySQL(environment = "prod")
+        #Conexão com o BD
         if environment == "prod"
             host = "10.55.192.17";
             user = "vitor.diniz";
@@ -22,6 +23,7 @@ module DBAccess
 
 
     function executeQuery(query, env = "Prod")
+        #Envia consulta ao BD
         connection = connectMySQL(env);
         result = DBInterface.execute(connection, query);
         result = DataFrame(result)
@@ -31,21 +33,30 @@ module DBAccess
 
 
     function getAccount(code ::String)
+        #Busca uma conta específica de todas as empresas
         query = open(f->read(f, String), "../SQL/single_account_query.sql")
         query = replace(query, ":code:" => code)  
         return executeQuery(query) 
     end
 
     function getDF(cvm ::String)
+        #Busca demonstrativos de uma determidada empresa
         query = open(f->read(f, String), "../SQL/getDF.sql")
         query = replace(query, ":cvm:" => cvm) 
         return executeQuery(query) 
     end
 
     function getTickers() ::DataFrame
+        #Busca lista de tickers
         query = open(f->read(f, String), "../SQL/getTickers.sql")
         return executeQuery(query, "hml")
-    end    
+    end
+
+    function getStocks() ::DataFrame
+        #Busca quantidades de ações para cada empresa
+        query = open(f->read(f, String), "../SQL/num_ações.sql")
+        return executeQuery(query, "hml")        
+    end
 end
 
 
