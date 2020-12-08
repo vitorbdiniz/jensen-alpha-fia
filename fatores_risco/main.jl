d213c31adfa5a0ec5b8ea6cf31494a5a830361b6
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-08 13:42:03 -0300

    Adiciona main

---
+++ fatores_risco/main.jl
@@ -0,0 +1,11 @@
+include("./risk_factors.jl");
+using .factors;
+
+function main()
+    r = riskFactors(true);
+    println(first(r,100));
+    
+end
+
+
+main();
--- fatores_risco/risk_factors.jl
+++ fatores_risco/risk_factors.jl
@@ -1,61 +1,86 @@
-#Libs
-using DataFrames, CSV, Dates;
+module factors
+    #Libs----
+    using DataFrames, CSV, Dates;
 
-#My Modules
-include("../MySQL/mysql_access.jl");
-include("./get_prices.jl");
-using .DBAccess, .pandas_datareader;
+    #My Modules----
+    include("../MySQL/mysql_access.jl");
+    include("./get_prices.jl");
+    using .DBAccess, .prices;
 
-#Functions
+    #Export functions----
+    export riskFactors;
 
-function riskFactors()
-    tickers ::DataFrame = getTickers();
-    CSV.write("../data/tickers.csv", tickers);
-    prices::DataFrame = getPrices(tickers);
-#    factors::DataFrame = calculateRiskFactors(prices);
-#    CSV.write("../data/riskFactors.csv", factors);
-#    return factors;
+    #Functions----
+
+    function riskFactors(test::Bool = false)
+        #Cálculo dos fatores de risco em base diária
+
+        #Consultas
+        tickers ::DataFrame = getTickers();
+        prices  ::DataFrame = getPrices(test);
+        stocks  ::DataFrame = getStocks();
+
+        #Processamento de dados
+        returns ::DataFrame = calculateReturns(prices);
+        calculatedFactors = returns;
+#        factors ::DataFrame = calculateRiskFactors(returns, stocks);
+#        CSV.write("../data/riskFactors.csv", factors);
+        return calculatedFactors;
+    end
+
+
+
+    function calculateRiskFactors(returns::DataFrame, stocks::DataFrame)::DataFrame
+        size::DataFrame = sizeFactor(returns, stocks);
+        value::DataFrame = valueFactor(returns, stocks);
+        liquidity::DataFrame = liquidityFactor(returns);
+        momentum::DataFrame = momentumFactor(returns);
+        market::DataFrame = marketFactor(returns);
+
+        resultRiskFactors ::DataFrame = appendDataFrames(size, value, liquidity, momentum, market);
+        return resultRiskFactors;
+    end
+
+    function sizeFactor(returns::DataFrame, stocks::DataFrame) ::DataFrame
+        #TODO
+
+    end
+    function valueFactor(returns::DataFrame, stocks::DataFrame) ::DataFrame
+        #TODO
+
+    end
+    function liquidityFactor(returns ::DataFrame)::DataFrame
+        #TODO    
+    end
+    function momentumFactor(returns ::DataFrame) ::DataFrame
+        #TODO
+    end
+    function marketFactor(returns ::DataFrame)::DataFrame
+        #TODO    
+        #Necessita portfolio
+    end
+
+
+    #Util Functions
+    function appendDataFrames(size::DataFrame, value::DataFrame, liquidity::DataFrame, momentum::DataFrame, market::DataFrame) ::DataFrame
+        #Realiza a junção de todos os dataframes de fatores de risco
+        result ::DataFrame;
+        append!(result, market);
+        append!(result, size);
+        append!(result, value);
+        append!(result, liquidity);
+        append!(result, momentum);
+        return result;
+    end
+
+    function calculateReturns(prices::DataFrame)
+        #Calcula retornos diários
+        ret::Array{Float64, 1} = [];
+        for i in 2:nrow(prices)
+            append!(ret, prices."Adj Close"[i]/prices."Adj Close"[i-1]-1);
+        end
+        delete!(prices, 1);
+        return DataFrame(codigo_cvm = prices."codigo_cvm", ticker_id = prices."ticker_id",data=prices."date",retornos = ret, volume = prices."Volume");
+    end
+
 end
-
-
-function calculateRiskFactors(prices::DataFrame)::DataFrame
-    size::DataFrame = sizeFactor(prices);
-    value::DataFrame = valueFactor(prices);
-    liquidity::DataFrame = liquidityFactor(prices);
-    momentum::DataFrame = momentumFactor(prices);
-    market::DataFrame = marketFactor(prices);
-
-    resultRiskFactors ::DataFrame = appendDataFrames(size, value, liquidity, momentum, market);
-    return resultRiskFactors;
-end
-
-function sizeFactor(prices::DataFrame) ::DataFrame
-    #TODO
-
-end
-function valueFactor(prices::DataFrame) ::DataFrame
-    #TODO
-    
-end
-function liquidityFactor(prices ::DataFrame)::DataFrame
-    #TODO    
-end
-function momentumFactor(prices ::DataFrame) ::DataFrame
-    #TODO
-end
-function marketFactor(prices ::DataFrame)::DataFrame
-    #TODO    
-    #Necessita portfolio
-end
-
-function appendDataFrames(size::DataFrame, value::DataFrame, liquidity::DataFrame, momentum::DataFrame, market::DataFrame) ::DataFrame
-    result ::DataFrame;
-    append!(result, market);
-    append!(result, size);
-    append!(result, value);
-    append!(result, liquidity);
-    append!(result, momentum);
-    return result;
-end
-
-riskFactors();
