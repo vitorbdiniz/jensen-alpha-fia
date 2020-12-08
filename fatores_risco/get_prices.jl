9f16d9d023bb7c2060dffab250641d6be6760f37
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-08 11:05:13 -0300

    Interface de preços jl <> py

--- fatores_risco/get_prices.jl
+++ fatores_risco/get_prices.jl
@@ -1,41 +1,12 @@
 
-
-module pandas_datareader
-    using PyCall, DataFrames, Dates;    
+module prices
+    using DataFrames, CSV;    
     export getPrices;
 
-    function getPrices(tickers) ::DataFrames.DataFrame
-        web = pyimport("pandas_datareader");
-        result = DataFrames.DataFrame();
-        errors::Array = [];
-        
-        for i in 1:length(tickers."codigo_negociacao")
-            ticker  = eraseCharacters(tickers."codigo_negociacao"[i], 1) * ".SA";
-            
-                println(string(i)*". Buscando dados de "*string(ticker));
-
-                a = web.get_data_yahoo(ticker, "2010-12-31", Dates.today())
-                 
-                println(a)
-                #prices::DataFrame = ;
-                #println(prices)
-                #append!(result, prices);
-            
-                println("--- Erro com dados de " *ticker)
-                #append!(errors, ticker);
-            
-
-
-        end
-        return result;
-    end
-
-    function eraseCharacters(str::String, i::Int) ::String
-        result::String = "";
-        for i in i+1:length(str)
-            result = result*str[i]
-        end
-        return result;
+    function getPrices(tickers) ::DataFrame
+        run(`python3 companies.py`);
+        prices::DataFrame = CSV.read("../data/prices.csv", DataFrame);
+        return prices;
     end
 
 end
