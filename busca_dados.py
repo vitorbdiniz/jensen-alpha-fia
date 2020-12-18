da0857e46a53b1cc3a98dd3bdb2aa986497dc805
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-18 08:18:02 -0300

    Adiciona funçao de busca da taxa Selic

--- busca_dados.py
+++ busca_dados.py
@@ -1,8 +1,11 @@
+#Python libs
 import datetime as dt
 import pandas as pd
 import pandas_datareader as web
 
+#Workspace libs
 import matrixDB
+import util
 
 
 
@@ -15,7 +18,7 @@ def get_prices(tickers, start=dt.date.today(), end=dt.date.today(), verbose = Fa
     '''
     if tickers == "all":
         tickers = matrixDB.get_tickers(environment = "prod", verbose = verbose)
-        
+
     i = 1
     prices = dict()
     for t in tickers:
@@ -28,3 +31,15 @@ def get_prices(tickers, start=dt.date.today(), end=dt.date.today(), verbose = Fa
             if verbose:
                 print("------- 404 -> Not found")
     return prices
+
+
+
+def getSelic(start = dt.date.today(), end = dt.date.today()):
+    start = util.dateReformat(start)
+    end = util.dateReformat(end)
+    url = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=csv&dataInicial="+ start +"&dataFinal="+end
+    selicBCB = pd.read_csv(url, sep=";")
+    selicBCB["valor"] = [ x/100 for x in util.reformatDecimalPoint(selicBCB["valor"], to=".")]
+    selicBCB["data"] = util.datesReformat(selicBCB["data"], False)
+    selic = pd.DataFrame({"valor":list(selicBCB["valor"])}, index = selicBCB["data"])
+    return selic
