97162c1e5cd7dae04b778f3528eb55aadd7e64ee
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-22 20:52:01 -0300

    Adiciona base de calculo

--- alpha.py
+++ alpha.py
@@ -3,10 +3,11 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import statistics as st
 import datetime as dt
-
+import util
 
 
 
 def jensens_alpha(risk_factors, portfolio_returns):
     pricing_model = LinearRegression().fit(risk_factors, portfolio_returns)
-    
+    return pricing_model
+
