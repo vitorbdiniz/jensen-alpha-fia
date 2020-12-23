ccf1057ab6b2c252efd85e8dcd07c82104c57e9e
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2020-12-23 15:07:35 -0300

    Leitura de FIs

---
+++ fundos_investimento.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def process_fis(fundos):
+    '''
+        Recebe um DataFrame com retornos de fundos e retorna um dicionário de fundos
+        chaves = codigos
+        valores = DataFrames
+    '''
+    fis = dict()
+    for i in range(fundos.shape[0]):
+        df = fundos.iloc[i]
+        if fundos["codigo"].iloc[i] not in fis:
+            fis[fundos["codigo"].iloc[i]] = pd.DataFrame()
+        fis[fundos["codigo"].iloc[i]] = fis[fundos["codigo"].iloc[i]].append(df, ignore_index=True)
+    return fis
