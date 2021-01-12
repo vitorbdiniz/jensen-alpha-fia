9ef99f59ba63c09dbf1b5a49005e7b33621c1019
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-01-12 11:22:27 -0300

    Fix bugs

--- fundos_investimento.py
+++ fundos_investimento.py
@@ -1,16 +1,31 @@
 import pandas as pd
+import numpy as np
 
-def preprocess_fis(fundos):
+def preprocess_fis(fundos=pd.DataFrame(), verbose = False):
     '''
         Recebe um DataFrame com retornos de fundos e retorna um dicionário de fundos
         chaves = codigos
         valores = DataFrames
     '''
+    if verbose:
+        print("Pré-processando dados de fundos de investimento")
+        codigo = fundos["codigo"].loc[0]
+        j = 2
+        print("1. Pré-processando dados de "+fundos["fundo"].loc[0])
+
     fis = dict()
-    for i in range(len(fundos["codigo"])):
-        df = fundos.iloc[i]
-        if fundos["codigo"].iloc[i] not in fis:
-            fis[fundos["codigo"].iloc[i]] = pd.DataFrame()
-        fis[fundos["codigo"].iloc[i]] = fis[fundos["codigo"].iloc[i]].append(df, ignore_index=True)
+
+    for i in fundos.index:
+        if verbose:
+            if fundos["codigo"].loc[i] != codigo:
+                codigo = fundos["codigo"].loc[i]
+                print(str(j) + ". Pré-processando dados de " +fundos["fundo"].loc[i])
+                j += 1
+        row = fundos.loc[i]
+        if fundos["codigo"].loc[i] not in fis:
+            fis[fundos["codigo"].loc[i]] = pd.DataFrame(columns=fundos.columns)
+        fis[fundos["codigo"].loc[i]].loc[i] = row
+    if verbose:
+        print("-------------------------------------------------------------------------------------------")
     return fis
 
