53ecd7aad553bd92376e495af4bdb1a1b22ec9ef
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-02-02 11:32:06 -0300

    Adiciona busca por dados de lucratividade do QMJ

--- matrixDB.py
+++ matrixDB.py
@@ -61,6 +61,12 @@ def get_equity(environment = "prod", verbose = False):
 
 
 
+def get_profitability_data(environment = "prod", verbose = False):
+    query = open(realpath("./SQL/qmj_profitability.sql"), 'r').read()
+    profit = execute_sql(query, environment=environment, verbose=verbose)
+    profit = profit.rename(columns={0:"codigo_cvm",  1:"ticker",  2:"data_referencia",  3:"demonstrativo_id", 4:"ativos",  5:"receita",  6:"custos", 7:"ROE",  8:"ROA",  9:"depreciacao", 10:"wc"})    
+    profit["ticker"] = [x.replace("$", "") for x in profit["ticker"]]
+    return profit
 
 
     
