c81e4c8e4df57fcb38fcbeaa1892e72d3254dc35
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-02-17 11:42:10 -0300

    Retira frequencia

--- busca_dados.py
+++ busca_dados.py
@@ -10,7 +10,7 @@ import padding as pad
 
 
 
-def get_prices(tickers, start=dt.date.today(), end=dt.date.today(),verbose = 0, get_from="yahoo", freq="daily"):
+def get_prices(tickers, start=dt.date.today(), end=dt.date.today(),verbose = 0, get_from="yahoo"):
     '''
         Busca cotações de uma lista de ativos para um determinado intervalo de tempo
 
@@ -28,7 +28,7 @@ def get_prices(tickers, start=dt.date.today(), end=dt.date.today(),verbose = 0, 
     if get_from=="influx":
         prices = get_prices_from_influx(tickers, start, end, verbose)
     else:
-        prices = get_prices_from_yahoo(tickers, start, end, verbose, freq)
+        prices = get_prices_from_yahoo(tickers, start, end, verbose)
     
     pad.verbose("line", level=4, verbose=verbose)
 
@@ -37,10 +37,7 @@ def get_prices(tickers, start=dt.date.today(), end=dt.date.today(),verbose = 0, 
 def kill_units(tickers):
     return [t for t in tickers if len(t) == 5]
 
-def get_last_date(prices, start):
-    return prices["PETR4"]["Date"].iloc[-1] if "PETR4" in prices.keys() else start
-
-def get_prices_from_yahoo(tickers, start, end, verbose, freq):
+def get_prices_from_yahoo(tickers, start, end, verbose):
     prices = dict()
     i=1
     for t in tickers:
@@ -50,28 +47,13 @@ def get_prices_from_yahoo(tickers, start, end, verbose, freq):
         try:
             ticker = t if t[0] == '^' else str(t+".SA").upper()
             prices[t] = web.get_data_yahoo(ticker, start, end)
-            prices[t] = apply_frequency(prices[t], freq)
+            prices[t].index = [x.date() for x in list(prices[t].index)]
+            #prices[t] = apply_frequency(prices[t], freq)
         except:
             if verbose:
                 print("------- Ação não encontrada")
     return prices
 
-
-
-def apply_frequency(prices, freq):
-    freq_dict = {"D":1, "M":30, "Q":90,"Y":250}
-    prices_result = prices.copy()
-
-    liquid_days = [1 if prices["Volume"].loc[date] > 0 else 0   for date in prices.index]
-    liquid_days = util.trailing_sum(liquid_days, period=freq_dict[str(freq[0]).upper()])
-    liquid_days = [x / freq_dict[str(freq[0]).upper()] for x in liquid_days]
-    prices_result["liquid_days"] = liquid_days
-
-    prices_result["Volume"] = util.trailing_sum(prices["Volume"], period=freq_dict[str(freq[0]).upper()])
-
-    prices_result.index = [x.date() for x in list(prices_result.index)]
-
-    return prices_result
 
 
 def get_prices_from_influx(tickers, start, end, verbose):
@@ -117,24 +99,23 @@ def get_stockid(stockid, ticker):
             return stockid["Id"].iloc[i]
     return -1
 
-def getSelic(start = dt.date.today(), end = dt.date.today(), verbose = False, persist = False):
+def getSelic(start = dt.date.today(), end = dt.date.today(), verbose = 0, persist = False):
     pad.verbose("Buscando série histórica da Selic", level=5, verbose=verbose)
-    start = util.dateReformat(start)
-    end = util.dateReformat(end)
+    start = util.dateReformat(str(start))
+    end = util.dateReformat(str(end))
     url = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=csv&dataInicial="+ start +"&dataFinal="+end
-    try:
-        selicBCB = pd.read_csv(url, sep=";")
-        if persist:
-            selicBCB.to_csv("./data/selic.csv", sep=";")
-    except:
-        selicBCB = pd.read_csv("./data/selic.csv", sep=";")
-    selicBCB["valor"] = [ x/100 for x in util.reformatDecimalPoint(selicBCB["valor"], to=".")]
-    selicBCB["data"] = util.datesReformat(selicBCB["data"], False)
-    selic = pd.DataFrame({"valor":list(selicBCB["valor"])}, index = selicBCB["data"])
+
+    selic = pd.read_csv(url, sep=";")
+    
+    if "valor" in selic.columns:
+        selic["valor"] = [ x/100 for x in util.reformatDecimalPoint(selic["valor"], to=".")]
+        selic.index = util.datesReformat(selic["data"], False)
+        selic = pd.DataFrame({"valor":list(selic["valor"])}, index = selic.index)
+    else:
+        Warning("Servidor não retornou taxa Selic. Utilizando dados antigos.")
+        selic = pd.read_csv("./data/selic.csv", sep=";")
 
     if persist:
         selic.to_csv("./data/selic.csv")
     pad.verbose("line", level=5, verbose=verbose)
     return selic
-
-
