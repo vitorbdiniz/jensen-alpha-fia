8afa4f4a7c202f8e0bf29fa8508c1d1a29778fe8
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-03-04 08:40:02 -0300

    Trata erro com series vazias

--- carteira_tamanho.py
+++ carteira_tamanho.py
@@ -43,18 +43,21 @@ def get_market_caps(prices, dates, tickers, verbose=0):
     market_cap = pd.DataFrame(market_cap, index = dates)            
     return market_cap
 
-def get_company_market_caps(prices, stocks, dates, ticker, verbose=0):
+def get_company_market_caps(prices:pd.Series, stocks:pd.Series, dates, ticker, verbose=0):
     pad.verbose(f"Calculando Market Cap de {ticker}", level=5, verbose=verbose)
 
     stocks = stocks.dropna()
     prices = prices.dropna().rolling(window=21).mean().dropna()
     market_caps = pd.Series([],index=[])
 
-    for d in dates:
-        price = util.get_previous_data(prices, d)
-        n_stocks = util.get_previous_data(stocks, d)
-        market_cap = pd.Series([price * n_stocks], index=[d])
-        market_caps = market_caps.append(market_cap)
+    if len(prices) == 0 or len(stocks) == 0:
+        market_caps = pd.Series([0 for d in dates],index=dates)
+    else:
+        for d in dates:
+            price = util.get_previous_data(prices, d)
+            n_stocks = util.get_previous_data(stocks, d)
+            market_cap = pd.Series([price * n_stocks], index=[d])
+            market_caps = market_caps.append(market_cap)
 
     return market_caps
 
