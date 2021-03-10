82d4406fda8fdcf43b8c7350aec1a5a43bca0709
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-03-10 15:40:45 -0300

    Modulariza formaçao de carteiras

--- formacao_carteiras.py
+++ formacao_carteiras.py
@@ -1,19 +1,15 @@
 import datetime as dt
 import pandas as pd
 import numpy as np
-import statistics as st
-from scipy.stats import zscore
 
-import busca_dados
-import matrixDB
-import util
+from util import rearange_prices
 import padding as pad
 
 from carteira_tamanho import get_market_caps
 from carteira_valor import get_all_book_to_market
 from carteira_liquidez import get_liquidities
 from carteira_momentum import get_momentum
-
+from carteira_BAB import get_all_betas
 from fator_qualidade import carteiraQuality 
 
 """
@@ -22,22 +18,25 @@ from fator_qualidade import carteiraQuality
 
 """
 
-def forma_carteiras(prices, amostra_aprovada, quantile=1/3, start= dt.date.today(), end= dt.date.today(), verbose=0, persist=False):
+def forma_carteiras(prices, amostra_aprovada, quantile=1/3, start= dt.date.today(), end= dt.date.today(), verbose=0):
     carteiras = dict()
 
     #betas = getBeta(prices, amostra_aprovada,start, end, verbose)
     #betas.to_csv("./data/alphas/betas.csv")
     #betas = pd.read_csv("./data/alphas/betas.csv", index_col=0)
 
-    closing_prices = util.rearange_prices(prices, start, end, column = "Adj Close")
-    volumes = util.rearange_prices(prices, start, end, column = "Volume")
+    closing_prices = rearange_prices(prices, start, end, column = "Adj Close")
+    volumes = rearange_prices(prices, start, end, column = "Volume")
 
-    carteiras['size']       = monta_carteiras("tamanho", "big", "small", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
-    carteiras['value']      = monta_carteiras("valor", "high", "low", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
-    carteiras['liquidity']  = monta_carteiras("liquidez", "liquid", "iliquid", volumes, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
-    carteiras['momentum']   = monta_carteiras("momentum", "winner", "loser", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
-    #carteiras['BAB']        = monta_carteiras("bab", "high_beta", "low_beta", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
-    #carteiras['quality']    = monta_carteiras("qmj", "quality", "junk", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose, persist=persist)
+    carteiras['liquidity']  = monta_carteiras("liquidez", "illiquid", "liquid", {"volumes":volumes, "prices":closing_prices}, amostra_aprovada, quantile, rejected=[30, None], start=start, end=end, verbose=verbose)
+    carteiras['liquidity'].to_csv("./data/carteiras/liq.csv")
+    #carteiras['size']       = monta_carteiras("tamanho", "big", "small", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose)
+    #carteiras['value']      = monta_carteiras("valor", "high", "low", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose)
+    #carteiras['momentum']   = monta_carteiras("momentum", "winner", "loser", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose)
+    
+    #carteiras['BAB']        = monta_carteiras("bab", "high_beta", "low_beta", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose)
+    
+    #carteiras['quality']    = monta_carteiras("qmj", "quality", "junk", closing_prices, amostra_aprovada, quantile, start=start, end=end, verbose=verbose)
     
     return carteiras
 
@@ -46,7 +45,7 @@ def forma_carteiras(prices, amostra_aprovada, quantile=1/3, start= dt.date.today
     FORMAÇÃO DE CADA CARTEIRA
 
 """
-def monta_carteiras(nome_carteira, carteira_acima, carteira_abaixo, prices, amostra_aprovada, quantile, start=dt.date(2010, 1, 1), end = dt.date.today(), verbose = 0, persist=False):
+def monta_carteiras(nome_carteira, carteira_acima, carteira_abaixo, prices, amostra_aprovada, quantile, rejected = [0,None], start=dt.date(2010, 1, 1), end = dt.date.today(), verbose = 0):
     """
         Identifica a carteira que deve ser formada e busca o indicador referente a ela
 
@@ -60,139 +59,57 @@ def monta_carteiras(nome_carteira, carteira_acima, carteira_abaixo, prices, amos
         carteira = get_all_book_to_market(prices=prices, dates=amostra_aprovada.index, tickers=amostra_aprovada.columns, verbose=verbose)
     
     elif nome_carteira.lower() == "liquidez" or nome_carteira.lower() == "liquidity" or nome_carteira.lower() == "iml":
-        carteira = get_liquidities(volumes=prices, dates=amostra_aprovada.index, tickers=amostra_aprovada.columns, verbose=verbose)
+        carteira = get_liquidities(quotations=prices, dates=amostra_aprovada.index, tickers=amostra_aprovada.columns, verbose=verbose)
     
     elif nome_carteira.lower() == "momento" or nome_carteira.lower() == "momentum" or nome_carteira.lower() == "mom" or nome_carteira.lower() == "wml":
         carteira = get_momentum(prices=prices, dates=amostra_aprovada.index, tickers=amostra_aprovada.columns, verbose=verbose)
     
     elif nome_carteira.lower() == "beta" or nome_carteira.lower() == "bab" or nome_carteira.lower() == "betting against beta":
-        carteira = 0 #TODO
+        carteira = get_all_betas(prices=prices, start=start, end = end, method = "standard", verbose = verbose)
     
     elif nome_carteira.lower() == "quality" or nome_carteira.lower() == "qmj" or nome_carteira.lower() == "qualidade":
         carteira = 0 #TODO
     
     else:
         raise ValueError(f"nome_carteira mal especificado: nome_carteira = {nome_carteira}")
-
-    carteira = classificador_df(carteira, amostra_aprovada, q = quantile, acima = carteira_acima, abaixo = carteira_abaixo, verbose=verbose)
-
-    if persist:
-        carteira.to_csv(f"./data/carteiras/{nome_carteira}.csv")
-
+    
+    carteira.to_csv("./data/carteiras/liq.csv")
+    carteira = classificador_df(carteira, amostra_aprovada, q = quantile, acima = carteira_acima, abaixo = carteira_abaixo, rejected=rejected, verbose=verbose)
+    carteira.dropna(how="all", inplace=True)
+    
     pad.verbose('line', level=2, verbose=verbose)
     return carteira
 
 
+"""
 
+    CLASSIFICADOR DE CARTEIRAS
 
 """
 
-    NOT CHANGED YET:
-
-"""
-
-
-
-def carteiraBeta(prices, amostra_aprovada, betas,quantile = 1/3, start= dt.date.today(), end= dt.date.today(), years = 3, verbose=False):
-    '''
-        Classifica cada ativo por período (diário, trimestral ou anual) em "high_beta" ou "low_beta" de acordo com o beta
-    '''
-    pad.verbose("Montando carteiras de beta", level=3, verbose=verbose)
-
-    carteira_beta = pd.DataFrame(index=amostra_aprovada.index, columns=amostra_aprovada.columns)
-    i = 1
-    for period in amostra_aprovada.index:
-        pad.verbose(str(i)+". Montando carteiras de beta para o período " + str(period) + " ---- restam " + str(len(amostra_aprovada.index)-i), level=5, verbose=verbose)
-        i += 1
-        
-        b = []
-        for ticker in amostra_aprovada.columns:
-            if period in betas.index and amostra_aprovada[ticker].loc[period] and (betas[ticker].loc[period] != None and pd.notna(betas[ticker].loc[period])):
-                b.append( betas[ticker].loc[period] )
-            else:
-                b.append(None)
-
-        carteira_beta.loc[period] = classificar(b, quantile, "high_beta", "low_beta")
-
-    pad.get_line()
-
-    return carteira_beta
-
-def getBeta(prices, amostra_aprovada, start= dt.date.today(), end= dt.date.today(), verbose=False):
-    pad.verbose("Calculando betas", level=3, verbose=verbose)
-    pad.verbose("Calculando retornos necessários", level=4, verbose=verbose)
-
-    ibov = util.getReturns(busca_dados.get_prices("ibov", start, end)["^BVSP"])
-    
-    utilDays = util.getUtilDays(start, end)
-    returns = util.allReturns(prices)
-    betas_result = pd.DataFrame(index=utilDays)
-
-    i = 1
-    for ticker in returns.keys():
-        pad.verbose(str(i)+". Calculando beta de " + ticker + " ---- restam " + str(len(returns.keys()) - i), level=5, verbose=verbose)
-        i += 1
-
-        check_returns = validate_returns_dates(returns[ticker], ibov)
-        dates = set(check_returns.index)
-        betas = pd.Series()
-        j = 0
-        for d in utilDays:
-            d = str(d)
-            if j < 21 or d not in dates:
-                betas.loc[d] = 0
-                if d in dates:
-                    j+=1
-                continue
-            elif j < 500: #2y para 250 dias úteis:
-                stock = check_returns["stock"].iloc[0:j]
-                benchmark = check_returns["benchmark"].iloc[0:j]
-            else:
-                stock = check_returns["stock"].iloc[j-500:j]
-                benchmark = check_returns["benchmark"].iloc[j-500:j]
-            b = beta(Rm=benchmark, Ra=stock)
-            betas.loc[d] = b
-            j+=1
-        betas_result[ticker] = betas
-    return betas_result
-
-
-def beta(Rm, Ra):
-    return np.cov(Rm, Ra)[0][1]/st.variance(Rm)
-
-
-"""
-
-    CLASSIFICADORES
-
-"""
-
-def classificador_df(df, amostra_aprovada, q = 0.5, acima = 1, abaixo = -1, verbose=0):
+def classificador_df(df:pd.DataFrame, amostra_aprovada:pd.DataFrame, q = 0.5, acima = 1, abaixo = -1, rejected = [0,None], verbose=0):
     """
         Percorre um DataFrame classificando, linha a linha, os valores de cada coluna como acima ou abaixo da região interquantil
-
-        Pré-condição:
-            df.shape[0] == amostra_aprovada.shape[0]
-
         
         Retorna um DataFrame contendo as classificações
     """
-    if df.shape[0] != amostra_aprovada.shape[0]:
-        raise ValueError(f"df.shape != amostra_aprovada.shape: df.shape = {df.shape}; amostra_aprovada.shape = {amostra_aprovada.shape}")
-    if df.shape[1] != amostra_aprovada.shape[1]:
+    if df.shape[1] < amostra_aprovada.shape[1]:
         amostra_aprovada = amostra_aprovada[list(df.columns)]
-
     result = pd.DataFrame(index=df.index, columns=df.columns)
 
     for i in df.index:
         pad.verbose(f"Classificando carteiras para {i}", level=4, verbose=verbose)
-        result.loc[i] = classificador_lista(list(df.loc[i]), list(amostra_aprovada.loc[i]), q, acima, abaixo)
+        try:
+            data = amostra_aprovada.index.get_loc(i, method="pad")
+        except:
+            continue
+        result.loc[i] = classificador_lista(list(df.loc[i]), list(amostra_aprovada.iloc[data]), q, acima, abaixo, rejected)
 
     return result
 
 
 
-def classificador_lista(lista, amostra_aprovada, q = 0.5, acima = 1, abaixo = -1):
+def classificador_lista(lista, amostra_aprovada, q = 0.5, acima = 1, abaixo = -1, rejected = [0,None]):
     """
         Classifica os valores de uma lista em acima ou abaixo da região interquantil.
         Se o valor estiver dentro da região, retorna 0.
@@ -203,7 +120,7 @@ def classificador_lista(lista, amostra_aprovada, q = 0.5, acima = 1, abaixo = -1
         retorna uma lista de strings contendo as classificações
     """
 
-    aux = [lista[i] for i in range(len(lista)) if (lista[i] != 0 and lista[i] != None and amostra_aprovada[i] )]
+    aux = [lista[i] for i in range(len(lista)) if (lista[i] not in rejected and amostra_aprovada[i] )]
 
     if len(aux) == 0:
         result = [None for i in lista]   
