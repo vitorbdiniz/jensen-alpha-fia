6c03ac40b498c61a04f97a58c6d98862e8cd3c12
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-03-12 13:43:26 -0300

    Add TODOs

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main(routine = "factors_complete"):
 		A função main executará a rotina solicitada
 		lista de rotinas:
 			1. 'factors_complete' -> Calcula todos os 7 fatores para o período de 2010-01-01 até hoje
-
+			2. 'factors_update' -> TODO
+			3. 'alpha_complete' -> TODO
+			4. 'alpha_onedate' -> TODO
+			4. 'alpha_singleportfolio_complete' -> TODO
+			5. 'alpha_singleportfolio_onedate' -> TODO
 	"""
 	if routine == "factors_complete":
 		routines.factors_complete_routine(start=date(2010, 1, 1), end=date.today(), source = "yahoo", quantile = 1/3, criterio_liquidez = 0.8, test = False, verbose = 5, persist = True)
@@ -17,4 +21,3 @@ def main(routine = "factors_complete"):
 
 if __name__ == "__main__":
     main()
-	
