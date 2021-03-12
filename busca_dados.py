94bdab7eb35410aabad47a2402c1883ae2b1f492
Vítor Braga Diniz <vitor.diniz@tradersclub.com.br>
2021-03-12 10:25:05 -0300

    Fix bugs de Influx

--- busca_dados.py
+++ busca_dados.py
@@ -42,63 +42,77 @@ def get_prices_from_yahoo(tickers, start, end, verbose):
     prices = dict()
     i=1
     for t in tickers:
-        pad.verbose(str(i) + ". Buscando preços de " + str(t) + " ---- faltam " + str(len(tickers)-i), level=5, verbose=verbose)
+        pad.verbose(f"{i}. Buscando preços de {ticker} ---- faltam {len(tickers)-i}", level=5, verbose=verbose)
         i+=1
 
         try:
             ticker = t if t[0] == '^' else str(t+".SA").upper()
             prices[t] = web.get_data_yahoo(ticker, start, end)
+            prices[t]["Close"] = prices[t]["Adj Close"]
         except:
-            if verbose:
-                print("------- Ação não encontrada")
+            pad.verbose("-> Ação não encontrada", level=5, verbose=verbose)
     return prices
+
 
 
 
 def get_prices_from_influx(tickers, start, end, verbose):
     """
-        Busca cotações do Influx 
+        Busca cotações do Stocks, base de dados no BD Influx do TC 
     """
-    stockid = pd.read_csv("./data/stocks_id.csv")
-    stockid = stockid[["Id","InternalSymbol"]]
-    influx = pd.read_csv("./data/precos_influx.csv")
-    influx = influx[["stockid", "time","Open", "High", "Low", "Close", "Volume"]]
-    influx["Date"] = [ x[0:10] for x in influx["time"]]
+    import time
+    data = get_prices_data(verbose=verbose)
+
     prices = dict()
 
     i = 1
     for ticker in tickers:
-        if verbose:
-            print(str(i) + ". Buscando preços de " + str(ticker) + " ---- faltam " + str(len(tickers)-i))
-            i+=1
-        id = get_stockid(stockid, ticker)
-        data = {"Date":[],"Open":[], "High":[], "Low":[], "Close":[], "Volume":[], "Adj Close":[]}
-        found = False
-        j = 0
-        while j < influx.shape[0]:
-            if influx["stockid"].iloc[j] == id:
-                found = True
-                data["Date"].append(influx["Date"].iloc[j])
-                data["Open"].append(influx["Open"].iloc[j])
-                data["High"].append(influx["High"].iloc[j])
-                data["Low"].append(influx["Low"].iloc[j])
-                data["Close"].append(influx["Close"].iloc[j])
-                data["Adj Close"].append(influx["Close"].iloc[j])
-                data["Volume"].append(influx["Volume"].iloc[j])
-            elif found:
-                j = influx.shape[0]
-            j+=1
-        prices[ticker] = pd.DataFrame(data, index=data["Date"]).drop(columns="Date")
-
+        pad.verbose(f"{i}. Buscando preços de {ticker} ---- faltam {len(tickers)-i}", level=5, verbose=verbose)
+        i+=1
+        try:
+            df = data[data['ticker'] == ticker].copy()
+            df.index = pd.DatetimeIndex(df['time'])
+            df.drop(columns=['ticker', 'time'], inplace=True)
+            prices[ticker] = df
+        except:
+            pad.verbose("-> Ação não encontrada", level=5, verbose=verbose)
     return prices
 
-def get_stockid(stockid, ticker):
-    for i in range(len(stockid["InternalSymbol"])):
-        if stockid["InternalSymbol"].iloc[i] == ticker:
-            return stockid["Id"].iloc[i]
-    return -1
-
-
     
+def get_dict_id_ticker(id_to_ticker = True, verbose=0):
+    """
+        Cria um dicionário que mapeia o Id para o ticker do ativo se id_to_ticker == True;
+        Cria um dicionário que mapeia o ticker para o Id do ativo se id_to_ticker == False.
 
+        Retorna ponteiro para o dicionário dict() criado
+    """
+    pad.verbose(f"Criando dicionário para id_to_ticker = {id_to_ticker}", level=3, verbose=verbose)
 
+    stocks_id = pd.read_csv("./data/stocks_id.csv")[["Id", "InternalSymbol"]]
+
+    if id_to_ticker:
+        dictionary = { stocks_id["Id"].iloc[row] : stocks_id["InternalSymbol"].iloc[row] for row in range(stocks_id.shape[0])}
+    else:
+        dictionary = { stocks_id["InternalSymbol"].iloc[row] : stocks_id["Id"].iloc[row] for row in range(stocks_id.shape[0])}
+    return dictionary
+
+def get_prices_data(dictionary = None, verbose=0):
+    if not dictionary:
+        dictionary = get_dict_id_ticker(verbose=verbose)
+    pad.verbose(f"Buscando dados de cotações", level=3, verbose=verbose)
+
+    data = pd.read_csv("./data/precos_influx.csv")
+    data = data[["stockid", "time","Open", "High", "Low", "Close", "Volume"]]
+    
+    pad.verbose(f"Pré-processando dados de cotações", level=3, verbose=verbose)
+    data["time"] = [str_to_datetime(data["time"].iloc[i], datatype='date') for i in range(data.shape[0])]
+    data["ticker"] = [ dictionary[id] for id in data["stockid"]] 
+
+    data = data.drop(columns=['stockid'])
+    return data
+
+def str_to_datetime(string, datatype = 'datetime'):
+    result = dt.datetime.strptime(str(string), '%Y-%m-%dT%H:%M:%SZ')   
+    if datatype == 'date' or datatype == dt.date:
+        result = dt.date(year=result.year, month=result.month, day=result.day)
+    return result
