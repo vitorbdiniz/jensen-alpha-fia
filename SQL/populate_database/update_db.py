96cd657c959b0d78a4d71e89ddf97547d746e6f2
Vitor Diniz <vitor.diniz96@gmail.com>
2021-06-28 09:44:31 -0300

    Atualiza tickers de açoes

--- SQL/populate_database/update_db.py
+++ SQL/populate_database/update_db.py
@@ -6,7 +6,10 @@ from scripts.database import matrixDB
 from SQL.populate_database import connection
 
 def update_time_dimension(database=risk_factors_db(), end=dt.date.today(), verbose=0 ):
-    start = get_last_date(database, verbose=verbose) + dt.timedelta(days=1)
+    start = get_last_date(database, verbose=verbose)
+    if start == dt.datetime.today():
+        return False
+    start += dt.timedelta(days=1)
     date_range = pd.date_range(start, end, freq='D')
     values = ''
 
@@ -19,6 +22,35 @@ def update_time_dimension(database=risk_factors_db(), end=dt.date.today(), verbo
     connection.execute_DML(database, insert, verbose=verbose)
 
     return True
+
+
+def update_ticker_dimension(database=risk_factors_db(), exchange='B3', verbose=0 ):
+    
+    tickers = get_non_registered_tickers(database, verbose=verbose)
+    if len(tickers) == 0:
+        return False
+
+    values = ''
+    for i in range(len(tickers)):
+        ticker = tickers[i]
+        values += f"('{ticker}', '{exchange}')"
+        values += ';' if i == len(tickers)-1 else ','
+
+    insert="INSERT INTO ticker_dimension(`ticker`, `exchange`) VALUES " + values
+    connection.execute_DML(database, insert, verbose=verbose)
+
+    return True
+
+
+
+def get_non_registered_tickers(database, verbose=0):
+    tickers = [ x.replace("$", "") for x in list(matrixDB.get_tickers(environment = "prod", verbose = verbose)[3])]
+
+    df = connection.execute_query(database, query='SELECT ticker FROM ticker_dimension;', verbose=verbose)
+    tickers_registered = set(df[0])
+    non_registered_tickers = [ticker for ticker in tickers if ticker not in tickers_registered]
+    return non_registered_tickers
+
 
 def get_last_date(database, verbose=0):
     df = connection.execute_query(database, query='SELECT MAX(date_) FROM time_dimension;', verbose=verbose)
